6e84461d327f2b9218c10acaf95fd8e02f9bf585
muhamadzaman <zamandev.us@gmail.com>
2023-08-03 17:49:02 -0700

    OrderTable: Implemented order related frontend features

--- app/components/orders/table.jsx
+++ app/components/orders/table.jsx
@@ -1,26 +1,107 @@
-const OrderTable = ({ orders }) => (
-  <table className="table orders-table">
-    <thead>
-      <tr>
-        <th>Order #</th>
-        <th>Ordered at</th>
-        <th>Pick up at</th>
-        <th>Customer Name</th>
-        <th>Item</th>
-        <th>Qty</th>
-        <th>Status</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {orders.map((order) => (
-        <OrderRow order={order} />
-      ))}
-    </tbody>
-  </table>
-);
+import { useEffect, useState } from "react";
+
+const OrderTable = ({ orders }) => {
+  const [sortedOrders, setSortedOrders] = useState([]);
+  const [sortConfig, setSortConfig] = useState({
+    key: "",
+    direction: "",
+  });
+
+  useEffect(() => {
+    setSortedOrders(orders)
+  }, [orders])
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+    sortData(key, direction);
+  };
+
+
+  const sortData = (key, direction) => {
+    const sortedData = [...sortedOrders];
+    sortedData.sort((a, b) => {
+      if (a[key] < b[key]) {
+        return direction === "ascending" ? -1 : 1;
+      }
+      if (a[key] > b[key]) {
+        return direction === "ascending" ? 1 : -1;
+      }
+      return 0;
+    });
+    setSortedOrders(sortedData);
+  };
+
+  return (
+    <table className="table orders-table">
+      <thead>
+        <tr>
+          <SortableHeader
+            label="Order #"
+            sortKey="id"
+            sortConfig={sortConfig}
+            requestSort={requestSort}
+          />
+          <SortableHeader
+            label="Ordered at"
+            sortKey="created_at"
+            sortConfig={sortConfig}
+            requestSort={requestSort}
+          />
+          <SortableHeader
+            label="Pick up at"
+            sortKey="pick_up_at"
+            sortConfig={sortConfig}
+            requestSort={requestSort}
+          />
+          <SortableHeader
+            label="Customer Name"
+            sortKey="customer_name"
+            sortConfig={sortConfig}
+            requestSort={requestSort}
+          />
+          <th>Item</th>
+          <th>Qty</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        {sortedOrders.map((order) => (
+          <OrderRow order={order} key={order.id}/>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const SortableHeader = ({ label, sortKey, sortConfig, requestSort }) => {
+  const isSorting = sortConfig.key === sortKey;
+  const direction = isSorting ? sortConfig.direction : "ascending";
+
+  return (
+    <th onClick={() => requestSort(sortKey)}>
+      {label}{" "}
+      {isSorting && (
+        <span>{direction === "ascending" ? "▲" : "▼"}</span>
+      )}
+    </th>
+  );
+};
 
 const OrderRow = ({ order }) => {
+  const [status, setStatus] = useState(order.fulfilled)
+  const fullFillOrder = async (order_id) => {
+    const response = await fetch(`/api/orders/${order_id}`, {
+      method: "PATCH"
+    })
+    setStatus(response.status == 200 ? true : false)
+  }
+
   return (
     <tr>
       <td>{order.id}</td>
@@ -29,8 +110,12 @@ const OrderRow = ({ order }) => {
       <td>{order.customer_name}</td>
       <td>{order.item}</td>
       <td>{order.quantity}</td>
-      <td>{order.fulfilled ? `Fulfilled` : `In progress`}</td>
-      <td></td>
+      <td>{status ? `Fulfilled` : `In progress`}</td>
+      <td>
+        {order.fulfilled ? "" :
+
+          <button disabled={status} onClick={() => fullFillOrder(order.id)
+          }>FulFill</button>}</td>
     </tr>
   );
 };
